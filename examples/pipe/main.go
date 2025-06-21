292163b9a60a5dc6f5b724258f8a34c1341a176a
Tobias Gleiter <gleiter.tobias@outlook.de>
2025-06-20 20:48:17 -0400

    feat: added slog to the pipe pkg #11

--- examples/pipe/main.go
+++ examples/pipe/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/bit8bytes/gogantic/input/chat"
 	"github.com/bit8bytes/gogantic/llm"
@@ -14,8 +16,13 @@ import (
 
 func main() {
 	chatPrompt, _ := chat.New([]llm.Message{
-		{Role: "system", Content: "You are a helpful assistant that translates {{.inputLanguage}} to {{.outputLanguage}}."},
-		{Role: "user", Content: "{{.text}}"},
+		{
+			Role:    "system",
+			Content: "You are a helpful assistant that translates {{.inputLanguage}} to {{.outputLanguage}}."},
+		{
+			Role:    "user",
+			Content: "{{.text}}",
+		},
 	})
 
 	data := map[string]interface{}{
@@ -29,18 +36,20 @@ func main() {
 		panic(err)
 	}
 
-	llama3_8b_model := ollama.Model{
+	model := ollama.Model{
 		Model:   "llama3:8b",
 		Options: ollama.Options{NumCtx: 4096},
 		Stream:  false,
 	}
 
-	ollamaClient := ollama.New(llama3_8b_model)
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	llm := ollama.New(model)
 	var parser output.Parser[[]string] = &separator.Space{}
 
 	ctx := context.TODO()
-	pipe := pipe.New(messages, ollamaClient, parser)
+	pipe := pipe.New(messages, llm, parser)
 	result, _ := pipe.Invoke(ctx)
-	fmt.Println(result)
+	fmt.Println(*result)
 
 }
